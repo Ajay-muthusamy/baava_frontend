10ca6a9ed54e0b2e8ed39f67eba8cfa80447f840
Ajay-muthusamy <ajaym.22cse@kongu.edu>
2024-09-27 23:56:02 +0530

    price list change

--- src/component/shopping/ProductList.jsx
+++ src/component/shopping/ProductList.jsx
@@ -32,6 +32,7 @@ import PremiumUniqueSparklers from "../json/PremiumUniqueSparklers.json";
 import MatchBox from "../json/MatchBox.json";
 import PremiumRocket from "../json/PremiumRocket.json";
 import WhistlingShot from "../json/WhistlingShots.json";
+import Bijili from "../json/biliji.json";
 import { useNavigate } from "react-router-dom";
 
 const ProductList = () => {
@@ -66,6 +67,11 @@ const ProductList = () => {
     address: "",
   });
   const [products, setProducts] = useState({
+    Bijili: Bijili.map((product) => ({
+      ...product,
+      quantity: 0,
+      subtotal: 0,
+    })),
     premiumOneSound: PremiumOneSound.map((product) => ({
       ...product,
       quantity: 0,
@@ -364,6 +370,59 @@ const ProductList = () => {
           </thead>
           <tbody className="text-center">
             <tr>
+              <td colSpan="5" className=" font-bold text-lg  text-left p-4">
+                <span className="bg-blue-500 p-2 rounded-lg mt-2 text-white">
+                  Bijili
+                </span>
+              </td>
+            </tr>
+            {products.Bijili.map((data, index) => (
+              <tr key={index} className="bg-gray-50 hover:bg-gray-100">
+                <td className="border border-gray-300 px-4 py-2">
+                  <img
+                    src={data.image}
+                    alt="Product"
+                    className="w-12 h-12 object-cover mx-auto"
+                  />
+                </td>
+                <td className="border border-gray-300 px-4 py-2">
+                  {data.title}
+                  <h2 className="md:hidden px-4 py-2 font-bold text-blue-600">
+                    {data.price}
+                  </h2>
+                </td>
+                <td className="hidden md:block border border-gray-300 px-4 py-5 font-bold text-blue-600">
+                  Rs: {data.price} .00
+                </td>
+                <td className="border border-gray-300 px-4 py-2">
+                  <div className="flex justify-center items-center space-x-4">
+                    <button
+                      className="text-3xl font-bold px-3 py-1 transition"
+                      onClick={() =>
+                        handleQuantityChange("Bijili", index, -1)
+                      }
+                    >
+                      -
+                    </button>
+
+                    <h2 className="text-2xl">{data.quantity}</h2>
+
+                    <button
+                      className="text-3xl font-bold px-1 py-1 transition"
+                      onClick={() =>
+                        handleQuantityChange("Bijili", index, 1)
+                      }
+                    >
+                      +
+                    </button>
+                  </div>
+                </td>
+                <td className="border border-gray-300 px-1 py-1">
+                  Rs: {data.subtotal} .00
+                </td>
+              </tr>
+            ))}
+            <tr>
               <td colSpan="5" className=" font-bold text-lg p-4 text-left ">
                 <span className="bg-blue-500 p-2 rounded-lg mt-2 text-white">
                   Premium One Sound
@@ -374,7 +433,7 @@ const ProductList = () => {
               <tr key={index} className="bg-gray-50 hover:bg-gray-100">
                 <td className="border border-gray-300 px-4 py-2">
                   <img
-                    src="https://via.placeholder.com/50"
+                    src={data.img}
                     alt="Product"
                     className="w-12 h-12 object-cover mx-auto"
                   />
@@ -429,7 +488,7 @@ const ProductList = () => {
               <tr key={index} className="bg-gray-50 hover:bg-gray-100">
                 <td className="border border-gray-300 px-4 py-2">
                   <img
-                    src="https://via.placeholder.com/50"
+                    src={data.img}
                     alt="Product"
                     className="w-12 h-12 object-cover mx-auto"
                   />
@@ -481,7 +540,7 @@ const ProductList = () => {
               <tr key={index} className="bg-gray-50 hover:bg-gray-100">
                 <td className="border border-gray-300 px-4 py-2">
                   <img
-                    src="https://via.placeholder.com/50"
+                    src={data.img}
                     alt="Product"
                     className="w-12 h-12 object-cover mx-auto"
                   />
@@ -1970,7 +2029,7 @@ const ProductList = () => {
           </div>
         </section>
       </div>
-      <ToastContainer/>
+      <ToastContainer />
     </div>
   );
 };
