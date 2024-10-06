b75f10c22620ed4b909d5e645cc4f14610999fea
Ajay-muthusamy <ajaym.22cse@kongu.edu>
2024-10-06 14:12:57 +0530

    price list change

--- src/component/shopping/ProductList.jsx
+++ src/component/shopping/ProductList.jsx
@@ -18,7 +18,6 @@ import FancyFountainPremium from "../json/FancyFountainPremium.json";
 import FancyChakaraPremium from "../json/FancyChakaraPremium.json";
 import FancyPencil from "../json/FancyPencil.json";
 import KidsNowelties from "../json/KidsNowelties.json";
-import GujarathPots from "../json/GujarathPots.json";
 import PremiumTwinklingStar from "../json/PremiumTwinklingStar.json";
 import ChottaSkyDisplayPremium from "../json/ChottaSkyDisplayPremium.json";
 import PremiumSkyDisplay from "../json/PremiumSkyDisplay.json";
@@ -31,7 +30,6 @@ import PremiumSparklers from "../json/PremiumSparklers.json";
 import PremiumUniqueSparklers from "../json/PremiumUniqueSparklers.json";
 import MatchBox from "../json/MatchBox.json";
 import PremiumRocket from "../json/PremiumRocket.json";
-import WhistlingShot from "../json/WhistlingShots.json";
 import Bijili from "../json/biliji.json";
 import { useNavigate } from "react-router-dom";
 
@@ -138,11 +136,6 @@ const ProductList = () => {
       quantity: 0,
       subtotal: 0,
     })),
-    GujarathPots: GujarathPots.map((product) => ({
-      ...product,
-      quantity: 0,
-      subtotal: 0,
-    })),
     PremiumTwinklingStar: PremiumTwinklingStar.map((product) => ({
       ...product,
       quantity: 0,
@@ -202,12 +195,7 @@ const ProductList = () => {
       ...product,
       quantity: 0,
       subtotal: 0,
-    })),
-    WhistlingShot: WhistlingShot.map((product) => ({
-      ...product,
-      quantity: 0,
-      subtotal: 0,
-    })),
+    }))
   });
   const validateForm = () => {
     const errors = {};
@@ -1137,61 +1125,6 @@ const ProductList = () => {
             <tr>
               <td colSpan="5" className=" font-bold text-lg  text-left p-4">
                 <span className="bg-blue-500 p-2 rounded-lg mt-2 text-white">
-                  GUJARATH POTS
-                </span>
-              </td>
-            </tr>
-            {products.GujarathPots.map((data, index) => (
-              <tr key={index} className="bg-gray-50 hover:bg-gray-100">
-                <td className="border border-gray-300 px-4 py-2">
-                  <img
-                    src={data.image}
-                    alt="Product"
-                    className="w-12 h-12 object-cover mx-auto"
-                    onClick={() => handleImageClick(data.image)}
-                  />
-                </td>
-                <td className="border border-gray-300 px-4 py-2">
-                  {data.title}
-                  <h2 className="md:hidden px-4 py-2 font-bold text-blue-600">
-                    ₹ {data.price}
-                  </h2>
-                </td>
-                <td className="hidden md:block border border-gray-300 px-4 py-5 font-bold text-blue-600">
-                  Rs: {data.price} .00
-                </td>
-                <td className="border border-gray-300 px-4 py-2">
-                  <div className="flex justify-center items-center space-x-4">
-                    <button
-                      className="text-3xl font-bold px-3 py-1 transition"
-                      onClick={() =>
-                        handleQuantityChange("GujarathPots", index, -1)
-                      }
-                    >
-                      -
-                    </button>
-
-                    <h2 className="text-2xl">{data.quantity}</h2>
-
-                    <button
-                      className="text-3xl font-bold px-1 py-1 transition"
-                      onClick={() =>
-                        handleQuantityChange("GujarathPots", index, 1)
-                      }
-                    >
-                      +
-                    </button>
-                  </div>
-                </td>
-                <td className="border border-gray-300 px-1 py-1">
-                  Rs: {data.subtotal} .00
-                </td>
-              </tr>
-            ))}
-
-            <tr>
-              <td colSpan="5" className=" font-bold text-lg  text-left p-4">
-                <span className="bg-blue-500 p-2 rounded-lg mt-2 text-white">
                   PREMIUM TWINKLING STAR
                 </span>
               </td>
@@ -1850,60 +1783,7 @@ const ProductList = () => {
               </tr>
             ))}
 
-            <tr>
-              <td colSpan="5" className=" font-bold text-lg  text-left p-4">
-                <span className="bg-blue-500 p-2 rounded-lg mt-2 text-white">
-                  WHISTLING SHOT
-                </span>
-              </td>
-            </tr>
-            {products.WhistlingShot.map((data, index) => (
-              <tr key={index} className="bg-gray-50 hover:bg-gray-100">
-                <td className="border border-gray-300 px-4 py-2">
-                  <img
-                    src={data.image}
-                    alt="Product"
-                    className="w-12 h-12 object-cover mx-auto"
-                    onClick={() => handleImageClick(data.image)}
-                  />
-                </td>
-                <td className="border border-gray-300 px-4 py-2">
-                  {data.title}
-                  <h2 className="md:hidden px-4 py-2 font-bold text-blue-600">
-                    ₹ {data.price}
-                  </h2>
-                </td>
-                <td className="hidden md:block border border-gray-300 px-4 py-5 font-bold text-blue-600">
-                  Rs: {data.price} .00
-                </td>
-                <td className="border border-gray-300 px-4 py-2">
-                  <div className="flex justify-center items-center space-x-4">
-                    <button
-                      className="text-3xl font-bold px-3 py-1 transition"
-                      onClick={() =>
-                        handleQuantityChange("WhistlingShot", index, -1)
-                      }
-                    >
-                      -
-                    </button>
-
-                    <h2 className="text-2xl">{data.quantity}</h2>
-
-                    <button
-                      className="text-3xl font-bold px-1 py-1 transition"
-                      onClick={() =>
-                        handleQuantityChange("WhistlingShot", index, 1)
-                      }
-                    >
-                      +
-                    </button>
-                  </div>
-                </td>
-                <td className="border border-gray-300 px-1 py-1">
-                  Rs: {data.subtotal} .00
-                </td>
-              </tr>
-            ))}
+           
           </tbody>
         </table>
 
