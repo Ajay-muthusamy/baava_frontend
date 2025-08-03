7299d0e0476638a8e0ccb2df89d292d1b8561f24
= <=>
2025-08-03 20:12:10 +0530

    p

--- src/component/shopping/ProductList.jsx
+++ src/component/shopping/ProductList.jsx
@@ -592,7 +592,7 @@ const ProductList = () => {
             <tr>
               <td colSpan="5" className=" font-bold text-lg  text-left p-4">
                 <span className="bg-blue-500 p-2 rounded-lg mt-2 text-white">
-                  BOMB PREMIUM
+                  PAPER BOMB
                 </span>
               </td>
             </tr>
