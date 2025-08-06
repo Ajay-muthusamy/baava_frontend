1718f62826991d04d150ebef55ceb4b7f1fd197b
= <=>
2025-08-06 22:46:09 +0530

    ph

--- src/component/shopping/ProductList.jsx
+++ src/component/shopping/ProductList.jsx
@@ -863,7 +863,7 @@ const ProductList = () => {
             <tr>
               <td colSpan="5" className=" font-bold text-lg  text-left p-4">
                 <span className="bg-blue-500 p-2 rounded-lg mt-2 text-white">
-                  GROUND CHAKARA PREMIUM
+                  NIGHT FANCY
                 </span>
               </td>
             </tr>
